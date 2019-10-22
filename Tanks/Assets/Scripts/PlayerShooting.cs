f2456795466be6337c0a6bc3a57706eb2bf0ad5d
kotokoman <53630864+kotokoman@users.noreply.github.com>
2019-10-22 01:30:53 -0300

    version 1 - start
    inicio do projeto
    
    - cena gameplay
    - tank: movimentação e shoot
    - camera
    - inicio UI

---
+++ Tanks/Assets/Scripts/PlayerShooting.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerShooting : MonoBehaviour
+{
+    public Rigidbody projetil;
+    public Transform saida;
+
+    public float shotSpeed = 15f;
+
+    private void Update()
+    {
+       if (Input.GetKey(KeyCode.Space))
+       {
+            Shoot();
+       }
+    }
+
+    void Shoot()
+    {
+       Rigidbody instanciaProjetil;
+       instanciaProjetil = Instantiate(projetil, saida.position, saida.rotation) as Rigidbody;
+       instanciaProjetil.velocity = shotSpeed * saida.forward;
+    }
+}
---
+++ Tanks/Assets/Scripts/TurretMovement.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TurretMovement : MonoBehaviour
+{
+
+    public float velocidade;
+
+    private Rigidbody m_Rigidbody;
+
+
+    private void Start()
+    {
+        m_Rigidbody = gameObject.GetComponent<Rigidbody>();
+    }
+
+    void Update()
+    {
+        Rotation();
+    }
+
+    void Rotation()
+    {
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            Vector3 rotation = transform.rotation.eulerAngles;
+            rotation.y = -0.5f;
+            transform.Rotate(rotation * velocidade * 2);
+        }
+
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            Vector3 rotation = transform.rotation.eulerAngles;
+            rotation.y = 0.5f;
+            transform.Rotate(rotation * velocidade * 2);
+        }
+    }
+}
